33e8befa638553dfcdb0738209e0da514c31aaec
Shalabh <shalabh.aggarwal@openlabs.co.in>
2011-07-04 18:18:03 +0530

    License correction and refactoring

--- shipping_register.py
+++ shipping_register.py
@@ -2,7 +2,7 @@
 """
     Shipping register
         
-    :copyright: (c)2010-2011, Open Labs Business Solution
+    :copyright: (c) 2010-2011, Open Labs Business Solution
     :copyright: (c) 2011 by Openlabs Technologies & Consulting (P) Ltd.
     
     :license: AGPL, see LICENSE for more details.
--- stock.py
+++ stock.py
@@ -1,27 +1,13 @@
 # -*- coding: UTF-8 -*-
 """
-Inherits stock.picking to create a new page for 'Shipping Information'
-which appears only in 'done' state.
+    Inherits stock.picking to create a new page for 'Shipping Information'
+    which appears only in 'done' state.
+        
+    :copyright: (c) 2010-2011, Open Labs Business Solution
+    :copyright: (c) 2011 by Openlabs Technologies & Consulting (P) Ltd.
+    
+    :license: AGPL, see LICENSE for more details.
 """
-#########################################################################
-#                                                                       #
-# Copyright (c) 2010 Open Labs Business Solutions                       #
-# Copyright (c) 2010-2011 Openlabs Technologies & Consulting (P) LTD    #
-#                                                                       #
-# This program is free software: you can redistribute it and/or modify  #
-# it under the terms of the GNU General Public License as published by  #
-# the Free Software Foundation, either version 3 of the License, or     #
-# (at your option) any later version.                                   #
-#                                                                       #
-# This program is distributed in the hope that it will be useful,       #
-# but WITHOUT ANY WARRANTY; without even the implied warranty of        #
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
-# GNU General Public License for more details.                          #
-#                                                                       #
-# You should have received a copy of the GNU General Public License     #
-# along with this program.  If not, see <http://www.gnu.org/licenses/>. #
-#########################################################################
-
 from osv import osv, fields
 
 
