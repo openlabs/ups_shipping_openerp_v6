3d017a1d83d574e596bb17453ca3f63310c15f50
Sharoon Thomas <sharoon.thomas@openlabs.co.in>
2011-07-04 19:13:18 +0530

    A sample UnitTest framework

---
+++ tests/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+    __init__
+
+    Test Suite for shipping
+
+    :copyright: (c) 2011 by Openlabs Technologies & Consulting (P) Limited
+    :license: GPL, see LICENSE for more details.
+"""
+
